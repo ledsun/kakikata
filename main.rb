aec45f6c3842ca63cec469b3d5e31f1d81fbe0e8
ledsun <shigeru.nakajima@gmail.com>
2024-04-23 23:07:30 +0900

    Make loader interface like the Zeitwerk

--- main.rb
+++ main.rb
@@ -1,37 +1,46 @@
 require 'js/require_remote'
 
 module OrbitalRing
-  refine String do
-    def to_snake_case = self.gsub(/([a-z\d])([A-Z])/, '\1_\2').downcase
+  module Util
+    refine String do
+      def to_snake_case = self.gsub(/([a-z\d])([A-Z])/, '\1_\2').downcase
+    end
+
+    refine Symbol do
+      def to_snake_case = self.to_s.to_snake_case
+    end
   end
 
-  refine Symbol do
-    def to_snake_case = self.to_s.to_snake_case
+  class Loader
+    using OrbitalRing::Util
+
+    def setup
+      # 定数名からモジュールをオートーロードします。
+      def Object.const_missing(id)
+        module_name = id.to_snake_case
+        JS::RequireRemote.instance.load(module_name)
+        p "#{module_name} loaded!"
+
+        mod = const_get(id)
+        # 読み込んだモジュールに、サブモジュールのオートーロードを定義します。
+        mod.define_singleton_method(:const_missing) do |sub_id|
+          path = self.name.to_s.split('::')
+                               .map(&:to_snake_case)
+                               .join('/')
+          module_name = "#{path}/#{sub_id.to_snake_case}"
+          JS::RequireRemote.instance.load(module_name)
+          p "#{module_name} loaded!"
+
+          const_get(sub_id)
+        end
+
+        mod
+      end
+    end
   end
 end
 
-using OrbitalRing
-
-# 定数名からモジュールをオートーロードします。
-def Object.const_missing(id)
-  module_name = id.to_snake_case
-  JS::RequireRemote.instance.load(module_name)
-  p "#{module_name} loaded!"
-
-  mod = const_get(id)
-  # 読み込んだモジュールに、サブモジュールのオートーロードを定義します。
-  mod.define_singleton_method(:const_missing) do |sub_id|
-    path = self.name.to_s.split('::')
-                         .map(&:to_snake_case)
-                         .join('/')
-    module_name = "#{path}/#{sub_id.to_snake_case}"
-    JS::RequireRemote.instance.load(module_name)
-    p "#{module_name} loaded!"
-
-    const_get(sub_id)
-  end
-
-  mod
-end
+loader = OrbitalRing::Loader.new
+loader.setup
 
 App.new
