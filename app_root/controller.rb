e5499f9bd53310327a50cb6357f13f8f964dca6c
ledsun <shigeru.nakajima@gmail.com>
2024-05-01 10:09:54 +0900

    名前空間を廃止

--- app_root/controller.rb
+++ app_root/controller.rb
@@ -1,12 +1,10 @@
-class App
-  class Controller
-    def initialize(view)
-      # ボタンを押したときの挙動を定義する
-      Document.querySelector('button').addEventListener 'click' do
-        statements = Document.querySelector '.statements'
-        phrase = statements[:value].to_s
-        view.update phrase
-      end
+class Controller
+  def initialize(view)
+    # ボタンを押したときの挙動を定義する
+    Document.querySelector('button').addEventListener 'click' do
+      statements = Document.querySelector '.statements'
+      phrase = statements[:value].to_s
+      view.update phrase
     end
   end
 end
--- app_root/view.rb
+++ app_root/view.rb
@@ -15,44 +15,42 @@ class JS::Object
   end
 end
 
-class App
-  class View
-    # 1文字分のHTMLテンプレート
-    CharactorTemplate = ERB.new(<<~'END_HTML')
-    <div class="character">
-      <span>
-        <%= character %>
-      </span>
+class View
+  # 1文字分のHTMLテンプレート
+  CharactorTemplate = ERB.new(<<~'END_HTML')
+  <div class="character">
+    <span>
+      <%= character %>
+    </span>
+  </div>
+  END_HTML
+
+  PageTemplate = ERB.new(<<~'END_HTML')
+  <div class="page">
+    <div class="grid">
+      <%= characters %>
     </div>
-    END_HTML
+  </div>
+  END_HTML
 
-    PageTemplate = ERB.new(<<~'END_HTML')
-    <div class="page">
-      <div class="grid">
-        <%= characters %>
-      </div>
-    </div>
-    END_HTML
+  def initialize(html_element)
+    @html_element = html_element
+  end
 
-    def initialize(html_element)
-      @html_element = html_element
-    end
+  # 画面に表示する
+  def update(phrase)
+    pages = []
+    phrase.gsub(/[^ぁ-んァ-ンー-龠々]/, '')
+          .chars
+          .each_slice(48) do |chars|
+            # 1ページに表示する文字数は48文字。
+            # 48文字に満たない場合に左寄せで表示されます。
+            # 足りない場合は空文字で埋めます。
+            chars = chars.fill(nil, chars.length..47)
+            pages << chars.map { |character| CharactorTemplate.result_with_hash character: }
+                          .join
+          end
 
-    # 画面に表示する
-    def update(phrase)
-      pages = []
-      phrase.gsub(/[^ぁ-んァ-ンー-龠々]/, '')
-            .chars
-            .each_slice(48) do |chars|
-              # 1ページに表示する文字数は48文字。
-              # 48文字に満たない場合に左寄せで表示されます。
-              # 足りない場合は空文字で埋めます。
-              chars = chars.fill(nil, chars.length..47)
-              pages << chars.map { |character| CharactorTemplate.result_with_hash character: }
-                            .join
-            end
-
-      @html_element.innerHTML = pages.map { |characters| PageTemplate.result_with_hash characters: }
-    end
+    @html_element.innerHTML = pages.map { |characters| PageTemplate.result_with_hash characters: }
   end
 end
