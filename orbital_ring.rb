d61e72f950643cb409e3148319bf44f662783812
ledsun <shigeru.nakajima@gmail.com>
2024-05-01 10:23:05 +0900

    OrbitalRingのロジックを読みやすく整頓する

--- orbital_ring.rb
+++ orbital_ring.rb
@@ -1,34 +1,35 @@
 require 'js/require_remote'
 
 module OrbitalRing
+  class Loader
+    attr_accessor :dir
+
+    def setup
+      Loader.define_const_missing @dir, Object
+    end
+
+    private
+
+    # const_missingを定義して、見つからない定数をリモートから読み込む
+    def self.define_const_missing(dir, mod)
+      mod.define_singleton_method(:const_missing) do |id|
+        # 定数名をスネークケースに変換して、リモートから読み込む
+        feature_name = Util.to_snake_case(id)
+        JS::RequireRemote.instance.load("#{dir}/#{feature_name}")
+
+        # 読み込んだモジュールにconst_missingを定義する
+        loaded_module = const_get(id)
+        Loader.define_const_missing dir, loaded_module
+        loaded_module
+      end
+    end
+  end
+
   module Util
     def self.to_snake_case(symbol)
       symbol.to_s
             .gsub(/([a-z\d])([A-Z])/, '\1_\2')
             .downcase
-    end
-  end
-
-  class Loader
-    attr_accessor :dir
-
-    def setup
-      Loader.define_const_missing Object, @dir
-    end
-
-    private
-
-    def self.define_const_missing(mod, dir)
-      mod.define_singleton_method(:const_missing) do |id|
-        module_name = Util.to_snake_case(id)
-
-        JS::RequireRemote.instance.load("#{dir}/#{module_name}")
-        p "#{module_name} loaded!"
-
-        loaded_module = const_get(id)
-        Loader.define_const_missing loaded_module, dir
-        loaded_module
-      end
     end
   end
 end
